c7d9085258e2a58a9d2f3f18a7a3c6a7a2f24a22
xunillinux <jonas@meise.ch>
2020-02-17 15:04:20 +0100

    added github pages deployment

---
+++ build/precache-manifest.896e856a1bfff2f7cc31970c0aa26eeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "969e6b43f69fb46af121fc36fffdda32",
+    "url": "/LifeOfPiBS/index.html"
+  },
+  {
+    "revision": "00829a72eeee95fe65b6",
+    "url": "/LifeOfPiBS/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fdc1f18d3b28483d7658",
+    "url": "/LifeOfPiBS/static/js/2.b38c7724.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/LifeOfPiBS/static/js/2.b38c7724.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00829a72eeee95fe65b6",
+    "url": "/LifeOfPiBS/static/js/main.276ccb87.chunk.js"
+  },
+  {
+    "revision": "aae0cfffd2b09063039b",
+    "url": "/LifeOfPiBS/static/js/runtime-main.ab08a057.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/LifeOfPiBS/static/media/logo.5d5d9eef.svg"
+  }
+]);
