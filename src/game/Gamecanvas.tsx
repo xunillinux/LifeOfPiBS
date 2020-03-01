8bc1e24e6f00ea7a9e26497aa356a1afebda56ef
Jonas Meise <jonas@meise.ch>
2020-03-01 17:16:21 +0100

    added gamecanvas implementation

---
+++ src/game/Gamecanvas.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Gamecanvas.css';
+import gamesketchimg from './gamesketchimg.jpg';
+
+class Canvas extends React.Component {
+
+    private gameCanvasRef: any;
+    private gameSketchImgRef: any;
+
+    constructor(props: any) {
+        super(props);
+        this.gameCanvasRef = React.createRef();
+        this.gameSketchImgRef = React.createRef();
+    }
+
+    componentDidMount() {
+        const canvas = this.gameCanvasRef.current;
+        const ctx = canvas.getContext("2d");
+        const img = this.gameSketchImgRef.current;
+
+        if(ctx != null && img != null && img.currentSrc != null){
+            img.onload = () => {
+                ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
+            }
+        }
+        
+    }
+
+    render() {
+
+        return (
+            <div>
+                <canvas ref={this.gameCanvasRef} width={755} height={425} />
+                <img ref={this.gameSketchImgRef} src={gamesketchimg} className="hidden" />
+            </div>
+        )
+    }
+}
+
+export default Canvas
