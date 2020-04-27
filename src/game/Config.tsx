5016adb1ee72d7551f39ab3ef260cf56eedd71aa
xunillinux <jonas@meise.ch>
2020-04-27 15:37:17 +0200

    added temporary ects animate

--- src/game/Config.tsx
+++ src/game/Config.tsx
@@ -1,7 +1,7 @@
 export default class Config {
 
     static canvasSize: any = {w:755, h:425};
-    static gravity: number = 0;
+    static gravity: number = 2;
     static fps: number = 30;
     static gameInterval: any;
 
