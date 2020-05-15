d56920e811c2232852ff9502a31fac9c3f7eb5b3
Joel <rueggerjoel@gmail.com>
2020-05-15 14:58:57 +0200

    added table for UI, added pictures for UI

--- src/game/GameUI.tsx
+++ src/game/GameUI.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './GameUI.css';
+import {Table} from 'react-bootstrap';
+import ectsCoin from '../game/images/ects_UI.jpg';
+import lives from '../game/images/lives_UI.jpg';
 
 interface IGameUIProps{
     currentLevelName: string;
@@ -21,7 +24,31 @@ export default class GameUI extends React.Component<IGameUIProps, IGameUIState> 
         return (
             <div className="GameUI">
               <header className="GameUI-header">
-                <div className="col-md-2">col-md-2</div>
+                <Table className="uiTable"size="sm" borderless>
+                  <tbody>
+                    <tr>
+                      <td></td>
+                      <td><img
+                        alt="logo"
+                        src={ectsCoin}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                      /> Score: {this.props.currentEctsScore}</td>
+                    </tr>
+                    <tr>
+                      <td>Level: {this.props.currentLevelName}</td>
+                      <td><img
+                        alt="logo"
+                        src={lives}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                      /> Lives: {this.props.currentLives}
+                      </td>
+                    </tr>
+                  </tbody>
+                </Table>
               </header>
             </div>
           );
