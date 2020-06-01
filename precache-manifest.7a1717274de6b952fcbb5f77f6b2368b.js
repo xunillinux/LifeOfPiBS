a4324c7acd8d47c851b3564d550bf5371cb68853
xunillinux <jonas@meise.ch>
2020-06-01 19:57:38 +0200

    Updates

---
+++ precache-manifest.7a1717274de6b952fcbb5f77f6b2368b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "729ad016830f2aa085716cfb035e3911",
+    "url": "/LifeOfPiBS/index.html"
+  },
+  {
+    "revision": "32c0cc07a1c9aa551c1b",
+    "url": "/LifeOfPiBS/static/css/main.0afd4d38.chunk.css"
+  },
+  {
+    "revision": "3e90d71397c11fc5ed68",
+    "url": "/LifeOfPiBS/static/js/2.6855aa5d.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/LifeOfPiBS/static/js/2.6855aa5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32c0cc07a1c9aa551c1b",
+    "url": "/LifeOfPiBS/static/js/main.0653f326.chunk.js"
+  },
+  {
+    "revision": "aae0cfffd2b09063039b",
+    "url": "/LifeOfPiBS/static/js/runtime-main.ab08a057.js"
+  },
+  {
+    "revision": "e913ebdcde950d8e9b9ecd221ba646a4",
+    "url": "/LifeOfPiBS/static/media/ARCADE_R.e913ebdc.TTF"
+  }
+]);
